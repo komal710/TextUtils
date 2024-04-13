e52caf4ba3a64b3e79e7fba56d1bdb3fa2bad015
Komal Singh <komalsingh7102000@gmail.com>
2024-04-13 19:06:26 +0530

    final change

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,13 +3,13 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
   const handleUpClick=()=>{
-    console.log("Uppercase was clicked" + text);
+    
     let newText = text.toUpperCase();
     setText(newText)
     props.showAlert("Converted to Upper Case","Success");
   }
   const handleLoClick=()=>{
-    console.log("Uppercase was clicked" + text);
+    
     let newText = text.toLowerCase();
     setText(newText)
     props.showAlert("Converted to Lower Case","Success");
@@ -30,7 +30,7 @@ const handleExtraSpaces = () => {
   props.showAlert("Removed Extra Space Case","Success");
 }
   const handleOnChange=(event)=>{
-    console.log("On Change");
+    
     setText(event.target.value);
    
   }
@@ -53,7 +53,7 @@ const handleExtraSpaces = () => {
   </div>
   <div className='container my-3' style={{color: props.mode==='dark'?'white':'#33667c'}}>
     <h2>Your text summary</h2>
-    <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+    <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
     <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to Read</p>
     <h2>Preview</h2>
     <p>{text.length>0?text:"Nothing to"}</p>
